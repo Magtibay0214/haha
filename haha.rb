52a9f5c55c768461570466413376099cba43eeb5
F227 <user@F228-24.(none)>
2014-04-30 11:29:46 +0800

    haha

---
+++ haha.rb
@@ -0,0 +1,30 @@
+require 'sinatra'
+
+get '/' do
+	erb :index
+end
+
+get '/hello/:name' do
+	@name = params[:name]
+	@age = params[:age]
+	@gender = params[:gender]
+	erb :hello
+end
+
+get '/search' do
+	erb :search_form
+end
+
+post '/results' do
+	@term = params[:term]
+	erb :results
+end
+
+
+get '/haha' do
+	"hahahahahahaha"
+end
+
+get '/hahaha' do
+	"You're in drugs"
+end
